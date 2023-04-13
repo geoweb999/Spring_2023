b4a4cb748981ab34b4eb2c344e397cde0ac7cd97
geoweb999 <55800507+geoweb999@users.noreply.github.com>
2023-04-13 13:34:51 -0700

    deque

---
+++ zzzStuff/src/zzzStuff/PriorityPair.java
@@ -0,0 +1,50 @@
+package zzzStuff;
+
+public class PriorityPair <T extends Comparable<? super T>, U extends Comparable<? super U>>{
+	
+	private T priority;
+	private U task;
+		
+	public PriorityPair(T first, U second) {
+		this.priority = first;
+		this.task = second;
+	}
+	
+	public T getFirst() {
+		return priority;
+	}
+	public void setFirst(T first) {
+		this.priority = first;
+	}
+	public U getSecond() {
+		return task;
+	}
+	public void setSecond(U second) {
+		this.task = second;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Priority: " + priority + " Task: " + task;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof PriorityPair<?, ?> otherPair) {
+//			Pair<?, ?> otherPair = (Pair<?, ?>) obj;
+			return (this.priority.equals(otherPair.priority) && this.task.equals(otherPair.task));
+		} else {
+			return false;
+		}
+	}
+	
+
+	public int compareTo(PriorityPair<T, U> other) {
+		int cmp = priority.compareTo(other.getFirst());
+        if (cmp != 0) {
+            return cmp;
+        }
+        return task.compareTo(other.getSecond());
+	}
+
+}
--- zzzStuff/src/zzzStuff/QueueTester.java
+++ zzzStuff/src/zzzStuff/QueueTester.java
@@ -7,9 +7,9 @@ import java.util.Queue;
 
 public class QueueTester {
 	public static void main(String[] args) {
-		Queue<Pair<Integer, String>> funque = new LinkedList<>();
-		funque.add(new Pair(1,"One"));
-		funque.add(new Pair(2,"Two"));
+		Queue<PriorityPair<Integer, String>> funque = new LinkedList<>();
+		funque.add(new PriorityPair(1,"Process2"));
+		funque.add(new PriorityPair(2,"Process1"));
 		System.out.println(funque);
 	}
 }
