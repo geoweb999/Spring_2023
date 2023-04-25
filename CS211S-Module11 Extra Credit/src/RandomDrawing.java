aad0ef5734387820e9a80c9f4b0c85ee35a78da8
geoweb999 <55800507+geoweb999@users.noreply.github.com>
2023-04-25 08:51:07 -0700

    M11

---
+++ CS211S-Module11 Extra Credit/src/RandomDrawing.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomDrawing<T> implements RandomDrawingInterface<T> {
+	
+
+	private List<T> drawingList;
+	public boolean allowsDuplicates;
+	private int size;
+	
+	
+	public RandomDrawing(boolean allowsDuplicates) {
+		List<T> newList = new ArrayList<>();
+		this.drawingList = newList;
+		size = 0;
+		this.allowsDuplicates = allowsDuplicates;
+	}
+	
+	public boolean addEntry(T entry) {
+		if (this.allowsDuplicates) {
+			if (drawingList.add(entry)) {
+				size++;
+				return true;
+			}
+		} else {
+			if (!drawingList.contains(entry)) {
+				if (drawingList.add(entry)) {
+					size++;
+					return true;
+				}
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public T selectWinner(boolean removeWinner) {
+		if (this.size == 0) {
+			return null;
+		}
+		
+		Random random = new Random();
+		int index = random.nextInt(this.size);
+		Object item = this.drawingList.get(index);
+		if (removeWinner) {
+			this.drawingList.remove(index);
+			size--;
+		}
+		return (T) item;
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+	public boolean allowsDuplicates() {
+		return this.allowsDuplicates;
+	}
+	
+	public void displayEntries() {
+		System.out.println("Drawing List allows duplicates=" + this.allowsDuplicates + ":");
+		for (T item : this.drawingList) {
+			System.out.println("\t"+item);
+		}
+	}
+	
+
+	
+
+}
