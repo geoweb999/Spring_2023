1dc3716aa00b875311878989ac640a896fb3ba8b
geoweb999 <55800507+geoweb999@users.noreply.github.com>
2023-05-05 14:38:26 -0700

    Project C

--- CS111C-Project C/src/BinarySearchTreeWithDups.java
+++ CS111C-Project C/src/BinarySearchTreeWithDups.java
@@ -21,9 +21,36 @@ public class BinarySearchTreeWithDups<T extends Comparable<? super T>> extends B
 
 	// IMPLEMENT THIS METHOD; THIS METHOD CANNOT BE RECURSIVE
 	private boolean addEntryHelperNonRecursive(T newEntry) {
-		// YOUR CODE HERE! 
-		
-		return false; // placeholder: replace with your own code
+		if (isEmpty()) {
+			return super.add(newEntry);
+		}
+	    BinaryNode<T> newNode = new BinaryNode<>(newEntry);
+		BinaryNode<T> currentNode = root;
+	    while (currentNode != null) {
+	        if (newEntry.compareTo(currentNode.getData()) < 0) {
+	            if (currentNode.getLeftChild() == null) {
+	                currentNode.setLeftChild(newNode);
+	                return true;
+	            } else {
+	                currentNode = currentNode.getLeftChild();
+	            }
+	        } else if (newEntry.compareTo(currentNode.getData()) > 0) {
+	            if (currentNode.getRightChild() == null) {
+	                currentNode.setRightChild(newNode);
+	                return true;
+	            } else {
+	                currentNode = currentNode.getRightChild();
+	            }
+	        } else {
+	            // element already exists in the tree
+	        	if (currentNode.getLeftChild() == null) {
+	                currentNode.setLeftChild(newNode);
+	                return true;
+	            } 
+	        	currentNode = currentNode.getLeftChild();
+	        }
+	    }
+		return false;
 	}
 
 	// THIS METHOD CANNOT BE RECURSIVE.
@@ -34,9 +61,16 @@ public class BinarySearchTreeWithDups<T extends Comparable<? super T>> extends B
 		// this initial code is meant as a suggestion to get your started- use it or delete it!
 		int count = 0;
 		BinaryNode<T> currentNode = root;
-
-		// consider a loop!
-		
+	    while (currentNode != null) {
+	        if (target.compareTo(currentNode.getData()) < 0) {
+                currentNode = currentNode.getLeftChild();
+	        } else if (target.compareTo(currentNode.getData()) > 0) {
+                currentNode = currentNode.getRightChild();
+	        } else {
+	        	count++;
+	        	currentNode = currentNode.getLeftChild();
+	        }
+	    }
 		return count;
 	}
 
@@ -45,13 +79,43 @@ public class BinarySearchTreeWithDups<T extends Comparable<? super T>> extends B
 	// Make sure to take advantage of the sorted nature of the BST!
 	public int countGreaterRecursive(T target) {
 		// YOUR CODE HERE! 
-			
+		if (isEmpty()) {
+			return 0;
+		}
 		// this initial code is meant as a suggestion to get your started- use it or delete it!
 		int count = 0;
 		BinaryNode<T> rootNode = root;
 				
-		// consider a helper method!
-			
+		count += countGreaterRecursiveHelper(target, root);
+		return count;
+	}
+	
+	private int countGreaterRecursiveHelper(T target, BinaryNode<T> currentNode) {
+		if (currentNode == null) {
+			return 0;
+		}
+		int count = 0;
+		int compare = currentNode.getData().compareTo(target);
+		if (compare < 0) {
+			if (currentNode.getRightChild() != null) {
+				count += countGreaterRecursiveHelper(target, currentNode.getRightChild());
+			}
+		} else if (compare > 0) {
+			count++;
+			if (currentNode.getLeftChild() != null) {
+				count += countGreaterRecursiveHelper(target, currentNode.getLeftChild());
+			}
+			if (currentNode.getRightChild() != null) {
+				count += countGreaterRecursiveHelper(target, currentNode.getRightChild());
+			}
+		} else {
+			if (currentNode.getLeftChild() != null) {
+				count += countGreaterRecursiveHelper(target, currentNode.getLeftChild());
+			}
+			if (currentNode.getRightChild() != null) {
+				count += countGreaterRecursiveHelper(target, currentNode.getRightChild());
+			}
+		}
 		return count;
 	}
 
@@ -66,9 +130,30 @@ public class BinarySearchTreeWithDups<T extends Comparable<? super T>> extends B
 		BinaryNode<T> rootNode = root;
 		Stack<BinaryNode<T>> nodeStack = new Stack<BinaryNode<T>>();
 		nodeStack.push(rootNode);
-
-		// consider a loop based on the stack!
-		
+		while (!nodeStack.isEmpty()) {
+			BinaryNode<T> currentNode = nodeStack.pop();
+			int compare = currentNode.getData().compareTo(target);
+			if (compare < 0) {
+				if (currentNode.getRightChild() != null) {
+					nodeStack.push(currentNode.getRightChild());
+				}
+			} else if (compare > 0) {
+				count++;
+				if (currentNode.getLeftChild() != null) {
+					nodeStack.push(currentNode.getLeftChild());
+				}
+				if (currentNode.getRightChild() != null) {
+					nodeStack.push(currentNode.getRightChild());
+				}
+			} else {
+				if (currentNode.getLeftChild() != null) {
+					nodeStack.push(currentNode.getLeftChild());
+				}
+				if (currentNode.getRightChild() != null) {
+					nodeStack.push(currentNode.getRightChild());
+				}
+			}
+		}
 		return count;
 	}
 			
@@ -78,8 +163,41 @@ public class BinarySearchTreeWithDups<T extends Comparable<? super T>> extends B
 	// The method can be iterative or recursive.
 	// If you make the method recursive, you might need to comment out the call to the method in Part B.
 	public int countUniqueValues() {
-		// YOUR EXTRA CREDIT CODE HERE! 
-		return 0; // placeholder: replace with your own code
+		if (isEmpty()) {
+			return 0;
+		}
+		List<T> list = new ArrayList<>();
+		inOrder(list, root);
+		int count = 1;
+		for (int i = 0; i < list.size() - 1; i++) {
+			if (list.get(i).compareTo(list.get(i+1)) < 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+   private void inOrder(List<T> traversal, BinaryNode<T> root) {
+	   if (root == null) {
+		   return;
+	   }
+	   inOrder(traversal, root.getLeftChild());
+	   traversal.add(root.getData());
+	   inOrder(traversal, root.getRightChild());
+   }
+	
+	public int countUniqueValuesHelper(BinaryNode<T> currentNode) {
+		int count = 0;
+		if (currentNode == null) {
+			return 0;
+		}
+		if (currentNode.getLeftChild() != null) {
+			count += countUniqueValuesHelper(currentNode.getLeftChild());
+		}
+		count++;
+		if (currentNode.getRightChild() != null) {
+			count += countUniqueValuesHelper(currentNode.getRightChild());
+		}
+		return count;
 	}
 
 }
